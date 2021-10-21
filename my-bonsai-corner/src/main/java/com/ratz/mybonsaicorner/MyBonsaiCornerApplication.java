ef1c5110cd378dfcd3ed547912b3768876f757d6
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-10-21 14:33:55 +0000

    Initial commit

---
+++ my-bonsai-corner/src/main/java/com/ratz/mybonsaicorner/MyBonsaiCornerApplication.java
@@ -0,0 +1,13 @@
+package com.ratz.mybonsaicorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBonsaiCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBonsaiCornerApplication.class, args);
+	}
+
+}
---
+++ my-bonsai-corner/src/test/java/com/ratz/mybonsaicorner/MyBonsaiCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratz.mybonsaicorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBonsaiCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
